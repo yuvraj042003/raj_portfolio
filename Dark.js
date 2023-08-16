3baf631fe74a3dc9f3d65de5678a1717b9c0e16a
Yuvraj Singh <81462095+yuvraj042003@users.noreply.github.com>
2023-08-16 22:00:29 +0530

    code

---
+++ Dark.js
@@ -0,0 +1,11 @@
+const Dark = () => {
+    const toggleTheme = () => {
+        alert("Hii")
+    }
+    return 
+  <>
+    <a href="#" class="btn" type="btn" onClick={()=> toggleTheme()} >ToogleTheme</a>
+  </>
+}
+
+export default Dark
